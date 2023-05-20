eea1b67f0959946f329a6fd15d0935b7a05411d1
Kdorina <kovacsdorina2@gmail.com>
2023-05-20 20:31:27 +0200

    Initial Commit

---
+++ Cube/CubeApp.swift
@@ -0,0 +1,17 @@
+//
+//  CubeApp.swift
+//  Cube
+//
+//  Created by Dorina Kovács on 2023. 05. 20..
+//
+
+import SwiftUI
+
+@main
+struct CubeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
